2de5179fb9b89ac7fb9013e09651382f56f22b8f
Tabitha Evangelista <165334183+tabevangelista@users.noreply.github.com>
2024-07-12 00:43:49 -0400

    Update NewEntry.java

--- NewEntry.java
+++ NewEntry.java
@@ -1,30 +1,43 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class NewEntry {
+public class NewEntry implements ActionListener {
 
     JFrame entryFrame;
     JPanel entryPanel;
     JTextArea entryText;
     JLabel entryLabel;
     JScrollPane entryScroll;
+    JMenuBar menuBar; 
+    JMenu file;
+    JMenuItem newEntry, openEntry, saveEntry, saveAsEntry, exitEntry;
+    public Object textArea;
+    NewFileFunction fileFunction;
     
     public NewEntry() {
 
         EntryFrame();
+        createMenuBar();
+        createFileMenu();
         EntryPanel();
         entryText();
-
+        
+        fileFunction = new NewFileFunction(this);
         entryFrame.setVisible(true);
         entryPanel.setVisible(true);
     }
 
     public void EntryFrame() {
-        
+        // create entry frame
         entryFrame = new JFrame();
         entryFrame.setSize(400, 400);
         entryFrame.setTitle("Journal");
@@ -63,7 +76,58 @@ public class NewEntry {
         entryScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         entryPanel.add(entryScroll);
     }
-}
 
-// possibly make a class for the save button 
-// learn how to have it save into files (using youtube video)
+    public void createMenuBar() {
+        // overall menu bar 
+        menuBar = new JMenuBar();
+        entryFrame.setJMenuBar(menuBar);
+
+        file = new JMenu("File");
+        menuBar.add(file);
+    }
+
+    public void createFileMenu() {
+        // file menu 
+        newEntry = new JMenuItem("New");
+        file.add(newEntry);
+
+        // make new a button 
+        newEntry.addActionListener(this);
+        newEntry.setActionCommand("New");
+
+        openEntry = new JMenuItem("Open");
+        file.add(openEntry);
+
+        // make an open button
+        openEntry.addActionListener(this);
+        openEntry.setActionCommand("Open");
+
+        saveEntry = new JMenuItem("Save");
+        file.add(saveEntry);
+        // make save a button
+
+        saveAsEntry = new JMenuItem("Save As"); 
+        file.add(saveAsEntry);
+        // make saveAs a button
+
+        exitEntry = new JMenuItem("Close"); 
+        file.add(exitEntry);
+        // make exit a button
+    }
+
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+
+        switch(command) { 
+            case "New": fileFunction.newFile(); 
+            break;
+            case "Open":fileFunction.openFile(); 
+            break;
+            
+        }
+        }
+    }
+
